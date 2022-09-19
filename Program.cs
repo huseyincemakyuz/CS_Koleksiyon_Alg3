bcffda138a7753c4762d6dff03c853ea402bb6ff
Huseyin Cem Akyuz <hsyncmakyz@gmail.com>
2022-09-19 19:55:44 +0300

    solved

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace koleksiyonlar_soru_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] vowels = {'a','e','ı','i','o','ö','u','ü'};
+
+            Console.WriteLine("Lütfen bir cümle yazınız:");
+            string sentence = Console.ReadLine().ToLower();
+
+            List<char> vowelsChars = new List<char>(){};
+
+            for(int i=0;i<sentence.Length;i++)
+            {
+                if(vowels.Contains(sentence[i]))
+                {
+                    vowelsChars.Add(sentence[i]);
+                }
+            }
+
+            Console.WriteLine("Sesli harfler:");
+            foreach (char vc in vowelsChars)
+            {
+                Console.Write(vc+" ");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Dizinin eleman sayısı: "+vowelsChars.Count);
+
+            
+        }
+    }
+}
